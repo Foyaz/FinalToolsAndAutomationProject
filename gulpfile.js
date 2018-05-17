f03622f06ab2ab47eb889d2a75736e63081009a0
Foyaz <kayserfoyez68@gmail.com>
2018-05-17 16:00:18 -0500

    Optimizing the images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
- 
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+
 gulp.task('css', () =>
     gulp.src('./src/css/*.css')
         .pipe(autoprefixer({
@@ -12,3 +14,9 @@ gulp.task('css', () =>
         }))
         .pipe(gulp.dest('./dist/css/'))
 );
+
+gulp.task('images', function () {
+    gulp.src('./src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+});
